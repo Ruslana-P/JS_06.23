ec8de4b2bcb4f1e13fd26ef2171c54baafa3b4a3
Ruslana-P <pavliuk_r_v@protonmail.com>
2023-06-01 11:47:52 +0200

    function switchPlaces

--- index.js
+++ index.js
@@ -15,3 +15,28 @@ function getDistance(x1, y1, x2, y2) {
   }
   return result;
 }
+
+function switchPlaces(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("Passed argument is not array");
+  }
+
+  if (arr.length === 0) {
+    return arr;
+  }
+
+  const middle = arr.length / 2;
+  let result;
+
+  if (Number.isInteger(middle)) {
+    const arr1 = arr.slice(0, middle);
+    const arr2 = arr.slice(middle, arr.length);
+    result = [...arr2, ...arr1];
+  } else {
+    const arr1 = arr.slice(0, middle);
+    const arr2 = arr.slice(middle, middle + 1);
+    const arr3 = arr.slice(middle + 1, arr.length);
+    result = [...arr3, ...arr2, ...arr1];
+  }
+  return result;
+}
