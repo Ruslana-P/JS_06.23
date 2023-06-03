c0cb9bac493c7389f21ebdfaf8e017bfc2aec7d2
Ruslana-P <pavliuk_r_v@protonmail.com>
2023-06-03 17:45:18 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let a = {
+  prop1: 11,
+  prop2: 22,
+};
+let c = {
+  prop1: 33,
+  prop2: 22,
+  prop4: 44,
+};
+
+let b = Object.assign(a, c);
+
+// console.log(b);
+// b.prop1 = 1;
+// b.prop2 = 2;
+console.log(b);
+// console.log(a);
